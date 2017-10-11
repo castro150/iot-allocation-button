0cb68195255d45d6baab0555f380a48e165aec91
Rafael Carneiro de Castro <rafaelcarneiroget@hotmail.com>
2017-10-11 19:37:01 -0300

    Attending

--- server/index.js
+++ server/index.js
@@ -114,6 +114,49 @@ router.get('/calls/attended', function(req, res, next) {
   });
 });
 
+router.put('/calls/attend', function(req, res, next) {
+  if (!req.body || !req.body.attendedBy) {
+    return res.status(400).json('Missing required body propertie.');
+  }
+
+  Call.find({
+    attended: false
+  }, null, {
+    sort: {
+      moment: 1
+    }
+  }, function(err, calls) {
+    if (err) {
+      return next(err);
+    }
+
+    let priorityCalls = calls.filter(function(call) {
+      return call.priority;
+    });
+
+    let notPriorityCalls = calls.filter(function(call) {
+      return !call.priority;
+    });
+
+    let orderedCalls = priorityCalls.concat(notPriorityCalls);
+    let toAttend = {};
+    if (orderedCalls.length > 0) {
+      toAttend = orderedCalls[0];
+      toAttend.attended = true;
+      toAttend.attendedBy = req.body.attendedBy;
+      toAttend.attendedTime = new Date();
+
+      Call.findByIdAndUpdate(toAttend._id, toAttend, {
+        new: true
+      }, function(err, attended) {
+        return res.status(200).json(attended);
+      });
+    } else {
+      return res.status(204).json();
+    }
+  });
+});
+
 //associate router to url path
 app.use('/api', router);
 
